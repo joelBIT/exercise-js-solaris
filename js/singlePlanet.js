a1cfa741922809ba6a7a47feece3036e32e59cce
Rotkeh <hektordavidsson@gmail.com>
2024-08-05 16:06:57 +0200

    singlePlanet 2

---
+++ js/singlePlanet.js
@@ -0,0 +1,192 @@
+"use strict";
+window.onload = () => {
+  getAPIKey();
+  document
+    .querySelector(".planet-home_button")
+    .addEventListener("click", () => {
+      console.log("Navigate back to main page");
+      window.location.href = "../index.html";
+    });
+  document
+    .querySelector(".planet-favourite_button")
+    .addEventListener("click", () => {
+      favouriteButtonClick();
+    });
+};
+async function getAPIKey() {
+  try {
+    let keyResponse = await fetch(
+      "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys",
+      {
+        method: "POST",
+      }
+    );
+
+    const key = await keyResponse.json();
+
+    let resp = await fetch(
+      "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies",
+      {
+        method: "GET",
+        headers: { "x-zocom": key.key },
+      }
+    );
+
+    const planets = await resp.json();
+    let id = 3;
+    localStorage.setItem("bodies", JSON.stringify(planets.bodies));
+    localStorage.setItem("activePlanetId", JSON.stringify(id));
+    displayPlanet();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function getActivePlanet() {
+  let activeId = JSON.parse(localStorage.getItem("activePlanetId"));
+  let activePlanet;
+  JSON.parse(localStorage.getItem("bodies")).forEach((planet) => {
+    if (planet.id == activeId) {
+      activePlanet = planet;
+    }
+  });
+  return activePlanet;
+}
+
+function displayPlanet() {
+  let activePlanet = getActivePlanet();
+
+  document.querySelector(".planet_name").textContent =
+    activePlanet.name.toUpperCase();
+  document.querySelector(".planet_latinName").textContent =
+    activePlanet.latinName.toUpperCase();
+  document.querySelector(".planet_text").textContent = activePlanet.desc;
+  document.querySelector(".planet-info_omkrets").textContent =
+    activePlanet.circumference + " km";
+  document.querySelector(".planet-info_maxTemp").textContent =
+    activePlanet.temp.day + " C";
+  document.querySelector(".planet-info_distans").textContent =
+    activePlanet.distance + " km";
+  document.querySelector(".planet-info_minTemp").textContent =
+    activePlanet.temp.night + " C";
+  const planetMoonsRef = document.querySelector(".planet-moons");
+  activePlanet.moons.forEach((moon) => {
+    let m = document.createElement("li");
+    m.textContent = moon;
+    planetMoonsRef.appendChild(m);
+  });
+
+  let color;
+  switch (activePlanet.id) {
+    case 0: {
+      color = "#FFD029";
+      break;
+    }
+    case 1: {
+      color = "#888888";
+      break;
+    }
+    case 2: {
+      color = "#E7CDCD";
+      break;
+    }
+    case 3: {
+      color = "#428ED4";
+      break;
+    }
+    case 4: {
+      color = "#EF5F5F";
+      break;
+    }
+    case 5: {
+      color = "#E29468";
+      break;
+    }
+    case 6: {
+      color = "#C7AA72";
+      break;
+    }
+    case 7: {
+      color = "#C9D4F1";
+      break;
+    }
+    case 8: {
+      color = "#7A91A7";
+      break;
+    }
+  }
+  let figure3 = document.querySelector(".planet-image_3");
+  figure3.style.backgroundColor = color;
+  let figure2 = document.querySelector(".planet-image_2");
+  figure2.style.backgroundColor = color + "1a";
+  let figure1 = document.querySelector(".planet-image_1");
+  figure1.style.backgroundColor = color + "0f";
+
+  updateButtonText();
+}
+
+function updateButtonText() {
+  if (isFavourite(getActivePlanet())) {
+    document.querySelector(".planet-favourite_button").textContent =
+      "REMOVE FROM FAVOURITES";
+  } else {
+    document.querySelector(".planet-favourite_button").textContent =
+      "ADD TO FAVOURITES";
+  }
+}
+
+function favouriteButtonClick() {
+  let planet = getActivePlanet();
+  if (isFavourite(planet)) {
+    removeFavourite(planet);
+    console.log("removed from favourites");
+  } else {
+    addPlanetToFavourites(planet);
+    console.log("added to favourites");
+  }
+  updateButtonText();
+}
+
+function isFavourite(planet) {
+  let favourites = JSON.parse(localStorage.getItem("favourites"));
+
+  if (favourites) {
+    if (favourites.length > 1) {
+      favourites.forEach((favourite) => {
+        if (favourite.id == planet.id) {
+          return true;
+        }
+      });
+    } else if (favourites.id == planet.id) {
+      return true;
+    }
+  } else {
+    return false;
+  }
+}
+
+function addPlanetToFavourites(planet) {
+  let favourites = JSON.parse(localStorage.getItem("favourites"));
+  if (favourites) {
+    favourites.push(planet);
+  } else {
+    favourites = planet;
+  }
+  localStorage.setItem("favourites", JSON.stringify(favourites));
+}
+
+function removeFavourite(name) {
+  let favourites = JSON.parse(localStorage.getItem("favourites"));
+  let index = -1;
+  if (favourites.length) {
+    for (let i = 0; i < favourites.length; i++) {
+      if (favourites[i].name == name) {
+        index = i;
+      }
+    }
+    favourites.splice(index, 1);
+    localStorage.setItem("favourites", JSON.stringify(favourites));
+  } else {
+    localStorage.removeItem("favourites");
+  }
+}
